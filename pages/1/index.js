dec8babc86f64346ba069e6db8b080744fc98a8c
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2020-10-21 06:50:51 +0200

    fix title tag

--- pages/1/index.js
+++ pages/1/index.js
@@ -8,13 +8,19 @@ import UserGroupIcon from "heroicons/outline/user-group.svg";
 
 export default function Copywork1() {
   return (
-    <div className="h-screen antialiased bg-gray-300">
-      <div className="grid justify-center h-full grid-flow-col gap-16 py-10">
-        <Sidebar />
-        <Sidebar variant="gray" />
-        <Sidebar variant="indigo" />
+    <>
+      <Head>
+        <title>Copywork: Sidebar navigation</title>
+      </Head>
+
+      <div className="h-screen antialiased bg-gray-300">
+        <div className="grid justify-center h-full grid-flow-col gap-16 py-10">
+          <Sidebar />
+          <Sidebar variant="gray" />
+          <Sidebar variant="indigo" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
@@ -22,71 +28,65 @@ function Sidebar({ variant }) {
   const { bg1, bg2, text1, text2 } = colors(variant);
 
   return (
-    <>
-      <Head>
-        <title>Copywork: Sidebar navigation</title>
-      </Head>
+    <nav
+      className={`flex flex-col h-full p-2 ${bg1} ${text1} rounded-lg shadow w-72`}
+    >
+      <ul className="font-medium">
+        <li className={`flex items-center p-2 ${text2} ${bg2} rounded-md`}>
+          <HomeIcon className="h-6" />
+          <span className="ml-3">Dashboard</span>
+        </li>
+        <li className="flex items-center p-2">
+          <UserGroupIcon className="h-6" />
+          <span className="ml-3">Team</span>
+        </li>
+        <li className="flex items-center p-2">
+          <CalendarIcon className="h-6" />
+          <span className="flex-1 ml-3">Calendar</span>
+          <span
+            className={`flex items-center justify-center h-6 px-3 text-sm font-semibold ${bg2} rounded-full`}
+          >
+            4
+          </span>
+        </li>
+        <li className="flex items-center p-2">
+          <InboxIcon className="h-6" />
+          <span className="flex-1 ml-3">Documents</span>
+          <span
+            className={`flex items-center justify-center h-6 px-3 text-sm font-semibold ${bg2} rounded-full`}
+          >
+            5
+          </span>
+        </li>
+        <li className="flex items-center p-2">
+          <ChartSquareBarIcon className="h-6" />
+          <span className="ml-3">Reports</span>
+        </li>
+      </ul>
 
-      <nav
-        className={`flex flex-col h-full p-2 ${bg1} ${text1} rounded-lg shadow w-72`}
-      >
-        <ul className="font-medium">
-          <li className={`flex items-center p-2 ${text2} ${bg2} rounded-md`}>
-            <HomeIcon className="h-6" />
-            <span className="ml-3">Dashboard</span>
-          </li>
-          <li className="flex items-center p-2">
-            <UserGroupIcon className="h-6" />
-            <span className="ml-3">Team</span>
-          </li>
-          <li className="flex items-center p-2">
-            <CalendarIcon className="h-6" />
-            <span className="flex-1 ml-3">Calendar</span>
-            <span
-              className={`flex items-center justify-center h-6 px-3 text-sm font-semibold ${bg2} rounded-full`}
-            >
-              4
-            </span>
-          </li>
-          <li className="flex items-center p-2">
-            <InboxIcon className="h-6" />
-            <span className="flex-1 ml-3">Documents</span>
-            <span
-              className={`flex items-center justify-center h-6 px-3 text-sm font-semibold ${bg2} rounded-full`}
-            >
-              5
-            </span>
-          </li>
-          <li className="flex items-center p-2">
-            <ChartSquareBarIcon className="h-6" />
-            <span className="ml-3">Reports</span>
-          </li>
+      <div className="flex-1 mt-8 font-medium">
+        <h2 className="px-3 text-xs tracking-wider uppercase">Projects</h2>
+        <ul className="mt-2">
+          <li className="px-3 py-2">Website redesign</li>
+          <li className="px-3 py-2">GraphQL API</li>
+          <li className="px-3 py-2">Customer migration guides</li>
+          <li className="px-3 py-2">Profit sharing program</li>
         </ul>
+      </div>
 
-        <div className="flex-1 mt-8 font-medium">
-          <h2 className="px-3 text-xs tracking-wider uppercase">Projects</h2>
-          <ul className="mt-2">
-            <li className="px-3 py-2">Website redesign</li>
-            <li className="px-3 py-2">GraphQL API</li>
-            <li className="px-3 py-2">Customer migration guides</li>
-            <li className="px-3 py-2">Profit sharing program</li>
-          </ul>
+      <div className="flex items-center p-3 font-medium">
+        <img
+          className="h-10 rounded-full"
+          src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2.3&w=256&h=256&q=80"
+          alt=""
+        />
+        <div className="flex-1 ml-4">
+          <div className={`${text2}`}>Dianne Robertson</div>
+          <div className="text-sm">View Profile</div>
         </div>
-
-        <div className="flex items-center p-3 font-medium">
-          <img
-            className="h-10 rounded-full"
-            src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2.3&w=256&h=256&q=80"
-            alt=""
-          />
-          <div className="flex-1 ml-4">
-            <div className={`${text2}`}>Dianne Robertson</div>
-            <div className="text-sm">View Profile</div>
-          </div>
-          <CogIcon className="h-6" />
-        </div>
-      </nav>
-    </>
+        <CogIcon className="h-6" />
+      </div>
+    </nav>
   );
 }
 
